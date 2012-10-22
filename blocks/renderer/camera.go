fecae4d99398386b9f7f6fbd3ee94fba37b49a8b
Sam Morrison <sam@sam.vg>
2012-10-21 19:16:17 -0700

    moved perspective matrix stuff to matrix util

--- blocks/renderer/camera.go
+++ blocks/renderer/camera.go
@@ -1,14 +1,13 @@
 package renderer
 
 import (
-	"math"
 	"sam.vg/goblocks/blocks/input"
 	"sam.vg/util/matrix"
 )
 
 type Camera struct {
 	modelViewMatrix  *matrix.Matrix4
-	projectionMatrix [16]float32
+	projectionMatrix *matrix.Matrix4
 	eyePosition      [3]float32
 }
 
@@ -22,40 +21,9 @@ func NewCamera(viewWidth, viewHeight int) *Camera {
 	return camera
 }
 
-const DegToRad = math.Pi / 180
-
 func (c *Camera) UpdateProjectionMatrix(width, height int) {
-	fov := 60.0 * DegToRad
-	near := 0.0625
-	far := 256.0
-
-	w, h := float64(width), float64(height)
-	r_xy_factor := math.Min(w, h) * 1.0 / fov
-	r_x := r_xy_factor / w
-	r_y := r_xy_factor / h
-	r_zw_factor := 1.0 / (far - near)
-	r_z := (near + far) * r_zw_factor
-	r_w := -2.0 * near * far * r_zw_factor
-
-	c.projectionMatrix[0] = float32(r_x)
-	c.projectionMatrix[1] = 0.0
-	c.projectionMatrix[2] = 0.0
-	c.projectionMatrix[3] = 0.0
-
-	c.projectionMatrix[4] = 0.0
-	c.projectionMatrix[5] = float32(r_y)
-	c.projectionMatrix[6] = 0.0
-	c.projectionMatrix[7] = 0.0
-
-	c.projectionMatrix[8] = 0.0
-	c.projectionMatrix[9] = 0.0
-	c.projectionMatrix[10] = float32(r_z)
-	c.projectionMatrix[11] = 1.0
-
-	c.projectionMatrix[12] = 0.0
-	c.projectionMatrix[13] = 0.0
-	c.projectionMatrix[14] = float32(r_w)
-	c.projectionMatrix[15] = 0.0
+	c.projectionMatrix = matrix.MakePerspectiveMatrix(width, height, 60.0, 0.0625, 256.0)
+	c.projectionMatrix.Transpose()
 }
 
 func (c *Camera) UpdateModelViewMatrix() {

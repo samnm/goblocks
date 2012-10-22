ca6a2ab77dd44bde8d114667f59906270ee5efaf
Sam Morrison <sam@sam.vg>
2012-10-21 21:43:21 -0700

    Better camera movement

--- blocks/renderer/camera.go
+++ blocks/renderer/camera.go
@@ -2,6 +2,7 @@ package renderer
 
 import (
 	"github.com/go-gl/glfw"
+	"math"
 	"sam.vg/goblocks/blocks/input"
 	"sam.vg/util/matrix"
 )
@@ -10,6 +11,8 @@ type Camera struct {
 	modelViewMatrix  *matrix.Matrix4
 	projectionMatrix *matrix.Matrix4
 	eyePosition      [3]float32
+	yaw, pitch       float32
+	mouseX, mouseY   float32
 }
 
 func NewCamera(viewWidth, viewHeight int) *Camera {
@@ -25,6 +28,7 @@ func NewCamera(viewWidth, viewHeight int) *Camera {
 func (c *Camera) Tick() {
 	moveSpeed := float32(0.1)
 	movement := [4]float32{}
+
 	if input.IsKeyDown(glfw.KeyUp) || input.IsKeyDown('W') {
 		movement[2] += moveSpeed
 	}
@@ -38,7 +42,10 @@ func (c *Camera) Tick() {
 		movement[0] += moveSpeed
 	}
 
-	m := matrix.MakeRotationMatrix(-input.MouseX()/100.0, 0, 1, 0)
+	c.UpdateRotation()
+
+	m := matrix.MakeRotationMatrix(c.pitch, 1, 0, 0)
+	m = m.Multiply(matrix.MakeRotationMatrix(c.yaw, 0, 1, 0))
 	movement = m.MultiplyPoint(movement)
 
 	c.eyePosition[0] += movement[0]
@@ -48,14 +55,26 @@ func (c *Camera) Tick() {
 	c.UpdateModelViewMatrix()
 }
 
+func (c *Camera) UpdateRotation() {
+	newMouseX, newMouseY := input.MouseX(), input.MouseY()
+
+	c.yaw += -(newMouseX - c.mouseX) / 100.0
+	c.pitch += -(newMouseY - c.mouseY) / 100.0
+
+	c.yaw = float32(math.Mod(float64(c.yaw), math.Pi*2))
+	c.pitch = float32(math.Max(-math.Pi/2, math.Min(math.Pi/2, float64(c.pitch))))
+
+	c.mouseX, c.mouseY = newMouseX, newMouseY
+}
+
 func (c *Camera) UpdateProjectionMatrix(width, height int) {
 	c.projectionMatrix = matrix.MakePerspectiveMatrix(width, height, 60.0, 0.0625, 256.0)
 	c.projectionMatrix.Transpose()
 }
 
 func (c *Camera) UpdateModelViewMatrix() {
-	c.modelViewMatrix = matrix.MakeRotationMatrix(-input.MouseY()/100.0, 1, 0, 0)
-	c.modelViewMatrix = c.modelViewMatrix.Multiply(matrix.MakeRotationMatrix(-input.MouseX()/100.0, 0, 1, 0))
+	c.modelViewMatrix = matrix.MakeRotationMatrix(c.pitch, 1, 0, 0)
+	c.modelViewMatrix = c.modelViewMatrix.Multiply(matrix.MakeRotationMatrix(c.yaw, 0, 1, 0))
 	c.modelViewMatrix = c.modelViewMatrix.Multiply(matrix.MakeTranslationMatrix(-c.eyePosition[0], -c.eyePosition[1], -c.eyePosition[2]))
 	c.modelViewMatrix.Transpose()
 }
